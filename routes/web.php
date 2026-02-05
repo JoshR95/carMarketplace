8dd2b7a971f6fea6a479dc215cdc3614be34b552
JoshR95 <joshuarickards@hotmail.co.uk>
2026-02-05 14:32:30 +0000

    routing lesson 1

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    return view('about');
+
+// instead of this get route to display the about page, we can use the view rout instead
+    // Route::get('/about', function() {
+    //     return view('about');
+    // });
+Route::view(uri: '/about', view:'about');
+
+// this route will return a page that returns the number given into the url after product/ and returns it as dynamic string displayed on the page 
+Route::get('/product/{id}', function (string $id) {
+    return "Product ID=$id";
 });
